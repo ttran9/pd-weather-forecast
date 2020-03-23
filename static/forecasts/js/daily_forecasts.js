2e15e553a2acf1c617a270095f8d240b9ac5c86d
jstran9 <wptran58@gmail.com>
2020-03-23 14:40:43 -0700

    modifying the function to grab content from JSON returned from DailyForecastListView.

--- static/forecasts/js/daily_forecasts.js
+++ static/forecasts/js/daily_forecasts.js
@@ -1,4 +1,26 @@
-function generateDailyForecasts(lowTemperatureList, highTemperatureList) {
+function generateDailyForecasts(dailyForecastList) {
+    var lowTemperatures = [];
+    var highTemperatures = [];
+
+    /*
+    * take the daily forecast content and put it into two separate lists.
+    * note: I tried to do this in the backend and I was having issues with the Paginator object so I decided to
+    * do some light parsing in the front-end.
+    */
+    for (var i = 0; i < dailyForecastList.length; i++) {
+        var dailyForecast = dailyForecastList[i];
+        var lowTempContent = dailyForecast.lowTemperature;
+        var highTempContent = dailyForecast.highTemperature;
+        lowTemperatures.push({
+            label: lowTempContent.label,
+            y: lowTempContent.y,
+        });
+        highTemperatures.push({
+            label: highTempContent.label,
+            y: highTempContent.y,
+        });
+    }
+
     var yAxisLabel = "Temperature (In Fahrenheit)";
     var xAxisLabel = "Time And Date";
     var themeColor = "light1";
@@ -10,8 +32,8 @@ function generateDailyForecasts(lowTemperatureList, highTemperatureList) {
     var highTemperatureChartTitle = "Daily Forecast (High Temperatures)";
 
     window.onload = function () {
-        var lowTempChart = generateForecastGraph(lowTemperatureList, lowTemperatureChartContainerName, lowTemperatureChartTitle, yAxisLabel, xAxisLabel, themeColor, isAnimationEnabled);
-        var highTempChart = generateForecastGraph(highTemperatureList, highTemperatureChartContainerName, highTemperatureChartTitle, yAxisLabel, xAxisLabel, themeColor, isAnimationEnabled);
+        var lowTempChart = generateForecastGraph(lowTemperatures, lowTemperatureChartContainerName, lowTemperatureChartTitle, yAxisLabel, xAxisLabel, themeColor, isAnimationEnabled);
+        var highTempChart = generateForecastGraph(highTemperatures, highTemperatureChartContainerName, highTemperatureChartTitle, yAxisLabel, xAxisLabel, themeColor, isAnimationEnabled);
         lowTempChart.render();
         highTempChart.render();
     }  
